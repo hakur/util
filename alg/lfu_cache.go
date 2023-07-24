81118c5cf1d3d91b198807941cad57997c9617b2
hakur <yuxing951@hotmail.com>
2023-07-24 16:02:58 +0800

    更新LFU缓存的delete方法

--- alg/lfu_cache.go
+++ alg/lfu_cache.go
@@ -7,17 +7,22 @@ import (
 )
 
 var (
-	ErrLFUCacheKeyNotFound = fmt.Errorf("key not found")
+	ErrLFUCacheKeyNotFound = fmt.Errorf("lfu cache key not found")
 )
 
+// NewLFUCache create new lfu cache with pre allocated size, but with some garbage collection, there is extra memory "in use"
+// NewLFUCache 新建固定大小的lfu缓存, 因为GC的问题导致看起来有一些额外的内存“占用”
 func NewLFUCache[KT string | int, VT any](size int) (t *LFUCache[KT, VT]) {
 	t = new(LFUCache[KT, VT])
 	t.size = size
-	t.frequency = make([]KT, size)
+	t.frequency = make([]KT, 0, size) // 保证slice的底层数组是同一个
+
 	t.data = make(map[KT]*LFUCacheNode[VT])
 	return t
 }
 
+// LFUCacheNode lfu cache node, use array instead of linked list, fast get key's value, but takes big cost when delete key
+// LFUCacheNode lfu缓存节点, 使用数组替代链表，快速取得key的value，但删除key的接口消耗巨大
 type LFUCacheNode[VT any] struct {
 	// Value 缓存存储的实际内容
 	Value VT
@@ -43,10 +48,20 @@ type LFUCache[KT string | int, VT any] struct {
 	// lock        sync.RWMutex // 并不是每个人都想要锁住的，看个人的编码情况自行决定是否加锁
 }
 
+// Len return length of current cache item count
+// Len 返回当前长度大小
+func (t *LFUCache[KT, VT]) Len() int {
+	return t.currentSize
+}
+
+// GetSize return max length of storage slots can use
+// GetSize 返回最大可用的存储槽个数
 func (t *LFUCache[KT, VT]) GetSize() int {
 	return t.size
 }
 
+// Put update or insert new key and value
+// Put 更新或插入新的键值对
 func (t *LFUCache[KT, VT]) Put(key KT, value VT) {
 	// t.lock.Lock()
 	// defer t.lock.Unlock()
@@ -59,6 +74,7 @@ func (t *LFUCache[KT, VT]) Put(key KT, value VT) {
 	slotIndex := t.currentSize
 	if t.currentSize >= t.size { // 满了就要找最后一位进行剔除,这会导致老key永远清不掉，而新key进来后下一次插入其他key则刚插入的key就立马被删除了
 		delete(t.data, t.frequency[t.currentSize-1])
+		t.frequency = append(t.frequency[:t.currentSize-1], t.frequency[t.currentSize:]...)
 	} else {
 		t.currentSize++
 	}
@@ -68,7 +84,8 @@ func (t *LFUCache[KT, VT]) Put(key KT, value VT) {
 		slotIndex = t.size - 1
 	}
 
-	t.frequency[slotIndex] = key
+	// t.frequency[slotIndex] = key
+	t.frequency = append(t.frequency, key)
 
 	t.data[key] = &LFUCacheNode[VT]{
 		Value:      value,
@@ -78,6 +95,7 @@ func (t *LFUCache[KT, VT]) Put(key KT, value VT) {
 	}
 }
 
+// Get get value by key name
 // Get 通过key取得value，返回值可能为零值或空值
 func (t *LFUCache[KT, VT]) Get(key KT) (value VT, err error) {
 	// t.lock.Lock()
@@ -91,7 +109,8 @@ func (t *LFUCache[KT, VT]) Get(key KT) (value VT, err error) {
 	}
 }
 
-// frequencyInc 增加频率
+// frequencyInc increase key use count and update access time
+// frequencyInc 增加key的频率和更新访问时间
 func (t *LFUCache[KT, VT]) frequencyInc(key KT) {
 	node := t.data[key]
 	if node != nil {
@@ -103,7 +122,10 @@ func (t *LFUCache[KT, VT]) frequencyInc(key KT) {
 			nodeSlotIndex := node.SlotIndex
 			if nodeSlotIndex > 0 {
 				prevSlotIndex := sort.Search(nodeSlotIndex, func(i int) bool {
-					prevNode := t.data[t.frequency[i]]
+					prevNode, ok := t.data[t.frequency[i]]
+					if !ok { // key的值被删除了
+						return true
+					}
 					if prevNode.Count < node.Count {
 						return true
 					} else if prevNode.Count == node.Count {
@@ -117,15 +139,22 @@ func (t *LFUCache[KT, VT]) frequencyInc(key KT) {
 				// 时间小于当前node，则当前node可以被认为是更活跃的，将当前node移动到data数组的左侧一位
 				if prevSlotIndex >= t.currentSize {
 					prevSlotIndex = t.currentSize - 1
+				} else if prevSlotIndex < 0 {
+					prevSlotIndex = 0
 				}
 
 				if prevSlotIndex != nodeSlotIndex {
-					prevNode := t.data[t.frequency[prevSlotIndex]]
-					prevNodeKey := t.frequency[prevSlotIndex]
-					t.frequency[prevSlotIndex] = key
-					t.frequency[nodeSlotIndex] = prevNodeKey
-					node.SlotIndex = prevSlotIndex
-					prevNode.SlotIndex = nodeSlotIndex
+					prevNode, ok := t.data[t.frequency[prevSlotIndex]]
+					if ok {
+						prevNodeKey := t.frequency[prevSlotIndex]
+						t.frequency[prevSlotIndex] = key
+						t.frequency[nodeSlotIndex] = prevNodeKey
+						node.SlotIndex = prevSlotIndex
+						prevNode.SlotIndex = nodeSlotIndex
+					} else {
+						t.frequency[prevSlotIndex] = key
+						node.SlotIndex = prevSlotIndex
+					}
 				}
 			}
 		} else {
@@ -134,22 +163,29 @@ func (t *LFUCache[KT, VT]) frequencyInc(key KT) {
 				prevSlotIndex := node.SlotIndex - 1
 				nodeSlotIndex := node.SlotIndex
 				if prevSlotIndex > -1 {
-					prevNode := t.data[t.frequency[prevSlotIndex]]
+					prevNode, found := t.data[t.frequency[prevSlotIndex]]
 					// 时间小于当前node，则当前node可以被认为是更活跃的，将当前node移动到data数组的左侧一位
 					var needSwitch bool
-					if prevNode.Count < node.Count {
+					if !found { // key的值被删除了
+						needSwitch = true
+					} else if prevNode.Count < node.Count {
 						needSwitch = true
 					} else if prevNode.Count == node.Count {
 						if prevNode.AccessTime < node.AccessTime {
 							needSwitch = true
 						}
 					}
-					if prevNode != nil && needSwitch {
-						prevNodeKey := t.frequency[prevSlotIndex]
-						t.frequency[prevSlotIndex] = key
-						t.frequency[nodeSlotIndex] = prevNodeKey
-						node.SlotIndex = prevSlotIndex
-						prevNode.SlotIndex = nodeSlotIndex
+					if prevSlotIndex != nodeSlotIndex && needSwitch {
+						if prevNode != nil {
+							prevNodeKey := t.frequency[prevSlotIndex]
+							t.frequency[prevSlotIndex] = key
+							t.frequency[nodeSlotIndex] = prevNodeKey
+							node.SlotIndex = prevSlotIndex
+							prevNode.SlotIndex = nodeSlotIndex
+						} else {
+							t.frequency[prevSlotIndex] = key
+							node.SlotIndex = prevSlotIndex
+						}
 					} else {
 						break
 					}
@@ -161,14 +197,37 @@ func (t *LFUCache[KT, VT]) frequencyInc(key KT) {
 	}
 }
 
+// GetTopHotKeys get most biggest access count of key
 // GetTopHotKeys 取得最热的几个key，热度按数组下标从左（小）到右（大）排列,如果要取得全部key的排列信息，则 topNum = GetSize() 即可
 func (t *LFUCache[KT, VT]) GetTopHotKeys(topNum int) (keys []KT) {
 	// t.lock.RLock()
 	// defer t.lock.RUnlock()
-	for i, key := range t.frequency {
-		if i < topNum {
-			keys = append(keys, key)
+	var count int
+	for _, key := range t.frequency {
+		if _, found := t.data[key]; found {
+			if count < topNum {
+				keys = append(keys, key)
+				count++
+			}
 		}
 	}
 	return
+}
+
+// Delete delete key and update cache storage, this methos takes big cost
+// Delete 删除key并更新缓存存储， 这个接口消耗是巨大的
+func (t *LFUCache[KT, VT]) Delete(key KT) {
+	var found bool
+	var node *LFUCacheNode[VT]
+	if node, found = t.data[key]; !found {
+		return
+	}
+	delete(t.data, key)
+	t.currentSize--
+
+	// very big cost
+	t.frequency = append(t.frequency[:node.SlotIndex], t.frequency[node.SlotIndex+1:]...)
+	for _, n := range t.data {
+		n.SlotIndex++
+	}
 }
--- alg/lfu_cache_test.go
+++ alg/lfu_cache_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestLFUCache(t *testing.T) {
+func TestLFUCachePut(t *testing.T) {
 	cache := NewLFUCache[int, int](4)
 	for i := 0; i < 100; i++ {
 		cache.Put(i, i)
@@ -35,6 +35,42 @@ func TestLFUCache(t *testing.T) {
 	cache.Put(96, 96)
 
 	assert.Equal(t, []int{99, 98, 97, 96}, cache.GetTopHotKeys(4))
+}
+
+func TestLFUCacheDelete(t *testing.T) {
+	cache := NewLFUCache[int, int](4)
+	for i := 0; i < 100; i++ {
+		cache.Put(i, i)
+	}
+
+	a, err := cache.Get(99)
+	assert.Equal(t, nil, err)
+	assert.Equal(t, 99, a)
+	a, err = cache.Get(10)
+	assert.Equal(t, ErrLFUCacheKeyNotFound, err)
+	assert.NotEqual(t, 10, a)
+
+	cache.Put(99, 99)
+	cache.Put(99, 99)
+	cache.Put(99, 99)
+	cache.Put(99, 99)
+	cache.Put(99, 99)
+	cache.Put(98, 98)
+	cache.Put(98, 98)
+	cache.Put(98, 98)
+	cache.Put(98, 98)
+	cache.Put(97, 97)
+	cache.Put(97, 97)
+	cache.Put(97, 97)
+	cache.Put(96, 96)
+	cache.Put(96, 96)
+
+	assert.Equal(t, []int{99, 98, 97, 96}, cache.GetTopHotKeys(4))
+
+	cache.Delete(97)
+	cache.Put(101, 101)
+
+	assert.Equal(t, []int{99, 98, 96, 101}, cache.GetTopHotKeys(4))
 }
 
 func BenchmarkLFUCachePut(b *testing.B) {
