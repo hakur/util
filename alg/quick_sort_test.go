e80653cf3b4d0bf487ea86d7ec6fa206a1360431
hakur <yuxing951@hotmail.com>
2024-03-12 11:28:21 +0800

    update quicksort test

--- alg/quick_sort_test.go
+++ alg/quick_sort_test.go
@@ -7,8 +7,8 @@ import (
 )
 
 func TestQuickSortNonR(t *testing.T) {
-	data := []int{12, 3, 7, 10, 15, 4, 9, 6, 5}
+	data := []int{12, 3, 15, 7, 10, 15, 4, 9, 6, 5}
 	// 3 7 12
 	QuickSortNonR(data)
-	assert.Equal(t, []int{3, 4, 5, 6, 7, 9, 10, 12, 15}, data)
+	assert.Equal(t, []int{3, 4, 5, 6, 7, 9, 10, 12, 15, 15}, data)
 }
